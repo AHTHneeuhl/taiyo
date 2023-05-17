7c642869745650394609fa54f40fbb6948bd884b
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-05-17 18:48:08 +0530

    feat: Create Historical Cases LineChart (#6)

--- src/components/Dashboard/CountryCases.tsx
+++ src/components/Dashboard/CountryCases.tsx
@@ -18,7 +18,17 @@ const CountryCases: React.FC = () => {
       <h2 className="text-center text-xl font-semibold mb-2 cursor-pointer hover:text-blue-700">
         Country Cases
       </h2>
-      <LineChart width={1024} height={480} data={countryCases?.countryCases}>
+      <LineChart
+        width={1024}
+        height={480}
+        data={countryCases?.countryCases}
+        margin={{
+          top: 5,
+          right: 0,
+          left: 30,
+          bottom: 5,
+        }}
+      >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="country" />
         <Tooltip />
