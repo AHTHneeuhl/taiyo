b6c55e79e49310244468f8af7abda5248b6cf396
Shubham Patrick <ahthneeuhl@gmail.com>
2023-05-17 20:47:18 +0530

    feat: No Contact Found

--- src/components/Contact/ContactList.tsx
+++ src/components/Contact/ContactList.tsx
@@ -1,3 +1,4 @@
+import { AiFillCloseCircle } from "react-icons/ai";
 import { useAppSelector } from "../../redux/store/hooks";
 import ContactCard from "./ContactCard";
 
@@ -5,17 +6,29 @@ const ContactList: React.FC = () => {
   const contacts = useAppSelector((state) => state.contacts.contacts);
 
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-      {contacts.map(({ id, firstName, lastName, status }) => (
-        <ContactCard
-          key={id}
-          id={id}
-          firstName={firstName}
-          lastName={lastName}
-          status={status}
-        />
-      ))}
-    </div>
+    <>
+      {contacts.length ? (
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+          {contacts.map(({ id, firstName, lastName, status }) => (
+            <ContactCard
+              key={id}
+              id={id}
+              firstName={firstName}
+              lastName={lastName}
+              status={status}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-row gap-3 border border-neutral-300 p-4 rounded-lg w-fit mx-auto">
+          <AiFillCloseCircle size={40} />
+          <p className="font-semibold text-neutral-700 text-center">
+            No Contact Found <br />
+            Please add contact from Create Contect Button
+          </p>
+        </div>
+      )}
+    </>
   );
 };
 
