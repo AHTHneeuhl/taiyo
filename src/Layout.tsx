c77a443ef9e37c714c1d1b2cdffd221d141c4345
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-05-17 15:59:22 +0530

    feat: create Sidebar component and Layout (#4)

---
+++ src/Layout.tsx
@@ -0,0 +1,18 @@
+import { Sidebar } from "./components/commmon";
+
+type TProps = {
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<TProps> = ({ children }) => {
+  return (
+    <div className="grid grid-cols-12 md:grid-cols-12">
+      <div className="md:col-span-1 bg-gray-200">
+        <Sidebar />
+      </div>
+      <div className="col-span-11 md:col-span-11">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/components/commmon/Sidebar.tsx
@@ -0,0 +1,20 @@
+import { useLocation } from "react-router-dom";
+import NavItem from "./NavItem";
+
+const Sidebar: React.FC = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <div className="flex flex-col gap-5 w-32 h-[100vh] bg-white shadow-md px-4 py-6">
+      <NavItem active={pathname === "/"} path="/">
+        Contacts
+      </NavItem>
+      <NavItem active={pathname === "/dashboard"} path="/dashboard">
+        Graphs & Charts
+      </NavItem>
+      <div className="mt-auto text-xl font-medium">Sidebar</div>
+    </div>
+  );
+};
+
+export default Sidebar;
